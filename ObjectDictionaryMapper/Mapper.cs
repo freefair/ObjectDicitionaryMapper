7476ab4ec36280cc3779ae4a9247e21ab38bdd21
BuildTools <unconfigured@null.spigotmc.org>
2018-10-20 17:08:35 +0200

    + icon

--- ObjectDictionaryMapper/Mapper.cs
+++ ObjectDictionaryMapper/Mapper.cs
@@ -1,9 +1,19 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace ObjectDictionaryMapper
 {
 	public class Mapper
 	{
-		public static Dictionary<string, object> 
+		public static Dictionary<string, object> ToDictionary(Object src)
+		{
+			return null;
+		}
+
+		public static T ToObject<T>(IDictionary dictionary)
+		{
+			return default(T);
+		}
 	}
 }
